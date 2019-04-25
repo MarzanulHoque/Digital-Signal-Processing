36eb6dc18344d2cc82a1678473deeb8544762b8d
MarzanulHoque <37240887+MarzanulHoque@users.noreply.github.com>
2019-04-24 23:02:26 -0700

    this is example of Discrete fouriour transformation

---
+++ DFT.m
@@ -0,0 +1,31 @@
+x = [2 3 -1 4];
+N = length(x);
+X = zeros(4,1)
+for k = 0:N-1
+    for n = 0:N-1
+        X(k+1) = X(k+1) + x(n+1)*exp(-j*pi/2*n*k)
+    end
+end
+
+t = 0:N-1;
+subplot(311)
+stem(t,x);
+xlabel('Time(s)');
+ylabel('Amplitude');
+title('Time Domain -Input sequence');
+
+subplot(312)
+stem(t,x);
+xlabel('Frequency');
+ylabel('|X(K)|');
+title('Frequency domain - Magnitude response ');
+
+subplot(313)
+stem(t,angle(X));
+xlabel('Frequency');
+ylabel('Phase')
+title('Frequency Domain - phase response')
+
+X
+angle(X)
+
