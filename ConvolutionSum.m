4b90a92680e65d11219ae722a145694c63bdda89
MarzanulHoque <37240887+MarzanulHoque@users.noreply.github.com>
2019-09-19 05:51:42 +0100

    Add files via upload

---
+++ ConvolutionSum.m
@@ -0,0 +1,48 @@
+x=input('Enter the sequence of x(n) : ');
+zerox = input('Zero position of x : ');
+h = input('Enter values of h(n) : ');
+zeroh = ('Enter the zero position of h : ');
+k = -(length(h)-1);
+k2 = length(x) + length(h) - 1;
+kstart = 1;
+kend = k2 - k1 + 1;
+
+xk = zeros(kstart,kend);
+hk = zeros(kstart,kend);
+
+for i=1:length(x)
+  xk(-k1+i-zerox+1) = x(i);
+endfor
+
+h = flip(h);
+for i=1:length(h)
+  hk(i+zeroh-1)=h(i);
+endfor
+
+shift = 0;
+for j=1:k2-zeroh+1+1
+    X(shift+1)=X(shift+1)+hk(i)*xk(i+shift);
+    for i=1:length(h)+zeroh-1
+      X(shift+1) = X(shift+1) + hk(i)*xk(i+shift);
+    endfor
+    shift = shift + 1;
+endfor
+
+disp('Value of x(k) : ');
+disp(xk);
+disp('Value of x(-h) : ');
+disp(hk);
+disp('Convolution : ');
+disp(X);
+
+subplot(3,1,1);
+stem(1:length(xk),xk);
+title('x(n)');
+
+subplot(3,1,2);
+stem(1:length(hk),hk);
+title('h(n)');
+
+subplot(3,1,3);
+stem(1:length(X),X);
+title('X(m)');
